85573eb2d9e2434c61d268a0f794b894734bd78c
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2022-04-18 12:39:32 -0400

    add activation functions

--- network.py
+++ network.py
@@ -3,3 +3,11 @@ the main module that handles the neural networks
 utilizes numpy for now
 """
 import numpy as np
+
+def LINEAR(inp):
+    return inp
+
+def SIGMOID(inp):
+    return(1/(1+np.exp(-inp)))
+
+ACTIVATIONS = {LINEAR,SIGMOID}
