7d74258b0021bcef35e2823ee700ec41e035bb22
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2022-06-02 23:05:49 -0400

    added some TODO's for later

--- network_base.py
+++ network_base.py
@@ -1,6 +1,8 @@
 """network.py
 contains an abstract base class for neural networks
 also has global variables for function names
+TODO: include the train and test net functions, as those seem agnostic...
+TODO: expand the base class in case there are parts where the subclasses have the same functions
 """
 import abc
 
